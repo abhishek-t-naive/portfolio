e7518ba807700922920319cef94731fc32112706
abhishekdtu6@gmail.com <abhishekdtu6@gmail.com>
2020-08-29 15:54:19 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-3032706a62022bb0a750.js"
+    "url": "webpack-runtime-dbf37fb8821a8eecffdc.js"
   },
   {
     "url": "framework-3f4ff05f42035c9ce86e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "060465ef995cfef71df1da5240e03c33"
+    "revision": "4fd4aa10fa48272bf0f896a654683ba4"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-48e3eaac8612807a1087.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "page-data/app-data.json",
-    "revision": "89af3ce2db8bb347d06e694e39c55b39"
+    "revision": "5e6398e90f0fe54d8fbdfc9234eb5794"
   },
   {
     "url": "polyfill-1b00869c7d6b5a191b2b.js"
